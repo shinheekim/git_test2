6ffb9aaa88a40cac357a5aeb4e487a0b83c0c07c
CINI <128473266+shinheekim@users.noreply.github.com>
2023-09-27 10:18:51 +0900

    Update Hello.java

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,13 +1,7 @@
 package git_test2;
 public class Hello {
- String message;
- public Hello() {
- this("안녕하세요");
+    String message = "안녕하세요";
+    public void say() {
+    System.out.println(message);
+    }
  }
- public Hello(String message) {
- this.message = message;
- }
- public void say() {
- System.out.println(message);
- }
-}
